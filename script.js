e5c7472a5f8d31dd2f7e8d94547e4083748378fc
jstevens515 <115838886+jeremystevens515@users.noreply.github.com>
2022-12-29 21:24:34 -0600

    added timer function to js file. added questions and answers to js file. added functions to dynamically set display to none and flex when needed.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // variables by parent
 // header
 var headerSection = document.querySelector('.header');
-var timerElement = document.querySelector('#countdown');
+var countdown = document.querySelector('#countdown');
 
 // main
 // welcome section
@@ -9,12 +9,13 @@ var welcomeSection = document.querySelector('.welcome');
 var startQuizButton = document.querySelector('.start-quiz-button');
 
 // question section
-var questionSection = document.querySelector('.question');
+var questionText = document.querySelector('.question');
+var questionText = document.querySelector('.question-text');
 var answer1Button = document.querySelector('#answer1');
 var answer2Button = document.querySelector('#answer2');
 var answer3Button = document.querySelector('#answer3');
 var answer4Button = document.querySelector('#answer4');
-var feedback = document.querySelector1('.feedback');
+var feedback = document.querySelector('.feedback');
 
 // final score section
 var finalScoreSection = document.querySelector('.final-score');
@@ -28,18 +29,72 @@ var clearHighscoresButton = document.querySelector('#clear-highscores');
 var goBackButton = document.querySelector('#go-back');
 
 
-timerElement.textContent = 75;
-
-// add eventlistener
-
 // create timer
+function timer() {
+    var timeLeft = 75;
+
+    var timerInterval = setInterval(function(){
+        if (timeLeft > 0) {
+            countdown.textContent = timeLeft;
+            timeLeft--;
+        } else {
+            clearInterval(timerInterval);
+        }
+    }, 1000);
+}
+
+// create function that sets display to none
+function displayNone (element) {
+    element.style.display = 'none';
+}
+
+function displayFlex (element) {
+    element.style.display = 'flex';
+}
 
 // write questions with answers
+// make sections visible
+function question1() {
+    questionText.textContent = 'Commonly used data types do NOT include: '
+    answer1Button.textContent = 'strings';
+    answer2Button.textContent = 'booleans';
+    answer3Button.textContent = 'alerts';
+    answer4Button.textContent = 'numbers';
+}
 
+function question2() {
+    questionText.textContent = 'The condition of an if/else statement is enclosed within _____.';
+    answer1Button.textContent = 'quotes';
+    answer2Button.textContent = 'curly brackets';
+    answer3Button.textContent = 'parentheses';
+    answer4Button.textContent = 'square brackets';
+}
+
+function question3() {
+    questionText.textContent = 'Arrays in JavaScript are used to store _____.';
+    answer1Button.textContent = 'numbers and strings';
+    answer2Button.textContent = 'other arrays';
+    answer3Button.textContent = 'booleans';
+    answer4Button.textContent = 'all of the above';
+}
+
+function question4() {
+    questionText.textContent = 'String values must be enclosed within _____ when being assigned to variables.';
+    answer1Button.textContent = 'commas';
+    answer2Button.textContent = 'curly brackets';
+    answer3Button.textContent = 'quotes';
+    answer4Button.textContent = 'parentheses';
+}
+
+function question5() {
+    questionText.textContent = 'A useful tool used during development and debugging for printing content to the debugger is:'
+    answer1Button.textContent = 'JavaScript';
+    answer2Button.textContent = 'terminal/bash';
+    answer3Button.textContent = 'for loops';
+    answer4Button.textContent = 'console.log';
+}
 // write logic for scoring quiz
 
-startQuizButton.addEventListener('click', sayHello);
 
-function sayHello() {
-    console.log('hello');
-}
+// add eventlisteners
+startQuizButton.addEventListener('click', timer);
