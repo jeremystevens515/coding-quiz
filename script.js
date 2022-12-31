82809e22ae3642f6624c276822477c96039c0cf1
jstevens515 <115838886+jeremystevens515@users.noreply.github.com>
2022-12-31 13:11:09 -0600

    experimented with grading logic

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// variables by parent
+// variables by parent, which store references to html elements
 // header
 var headerSection = document.querySelector('.header');
 var countdown = document.querySelector('#countdown');
@@ -9,7 +9,7 @@ var welcomeSection = document.querySelector('.welcome');
 var startQuizButton = document.querySelector('.start-quiz-button');
 
 // question section
-var questionText = document.querySelector('.question');
+var questionSection = document.querySelector('.question');
 var questionText = document.querySelector('.question-text');
 var answer1Button = document.querySelector('#answer1');
 var answer2Button = document.querySelector('#answer2');
@@ -28,73 +28,156 @@ var highscoreList = document.querySelector('.highscore-list');
 var clearHighscoresButton = document.querySelector('#clear-highscores');
 var goBackButton = document.querySelector('#go-back');
 
+// question objects
+var question1 = {
+    question: 'Commonly used data types do NOT include: ',
+    answer1: {
+        text: 'strings',
+        isCorrect: false
+    },
+    answer2: {
+        text: 'booleans',
+        isCorrect: false
+    },
+    answer3: {
+        text: 'alerts',
+        isCorrect: true
+    },
+    answer4: {
+        text: 'numbers',
+        isCorrect: false
+    }
+}
 
+var question2 = {
+    question: 'The condition of an if/else statement is enclosed within _____.',
+    answer1: {
+        text: 'quotes',
+        isCorrect: false
+    },
+    answer2: {
+        text: 'curly brackets',
+        isCorrect: false
+    },
+    answer3: {
+        text: 'parentheses',
+        isCorrect: true
+    },
+    answer4: {
+        text: 'square brackets',
+        isCorrect: false
+    }
+}
+
+var question3 = {
+    question: 'Arrays in JavaScript are used to store _____.',
+    answer1: {
+        text: 'numbers and strings',
+        isCorrect: false
+    },
+    answer2: {
+        text: 'other arrays',
+        isCorrect: false
+    },
+    answer3: {
+        text: 'booleans',
+        isCorrect: false
+    },
+    answer4: {
+        text: 'all of the above',
+        isCorrect: true
+    }
+}
+
+var question4 = {
+    question: 'String values must be enclosed within _____ when being assigned to variables.',
+    answer1: {
+        text: 'commas',
+        isCorrect: false
+    },
+    answer2: {
+        text: 'curly brackets',
+        isCorrect: false
+    },
+    answer3: {
+        text: 'quotes',
+        isCorrect: true
+    },
+    answer4: {
+        text: 'parentheses',
+        isCorrect: false
+    }
+}
+
+var question5 = {
+    question: 'A useful tool used during development and debugging for printing content to the debugger is: ' ,
+    answer1: {
+        text: 'JavaScript',
+        isCorrect: false
+    },
+    answer2: {
+        text: 'terminal/bash',
+        isCorrect: false
+    },
+    answer3: {
+        text: 'for loops',
+        isCorrect: false
+    },
+    answer4: {
+        text: 'console.log',
+        isCorrect: true
+    }
+}
+// default view
+function displayWelcomePage () {
+    questionSection.style.display = 'none';
+    finalScoreSection.style.display = 'none';
+}
 // create timer
 function timer() {
+    welcomeSection.style.display = 'none';
+    questionSection.style.display = 'flex';
     var timeLeft = 75;
 
     var timerInterval = setInterval(function(){
-        if (timeLeft > 0) {
+        if (timeLeft >= 0) {
             countdown.textContent = timeLeft;
             timeLeft--;
         } else {
             clearInterval(timerInterval);
+            
         }
     }, 1000);
 }
 
-// create function that sets display to none
-function displayNone (element) {
-    element.style.display = 'none';
+// display question
+function displayQuestion(object) {
+    feedback.textContent = '';
+    questionText.textContent = object.question;
+    answer1Button.textContent = object.answer1.text;
+    answer2Button.textContent = object.answer2.text;
+    answer3Button.textContent = object.answer3.text;
+    answer4Button.textContent = object.answer4.text;
 }
 
-function displayFlex (element) {
-    element.style.display = 'flex';
+// grade question
+function grading(object) {
+    answer1Button.addEventListener('click', isAns1Correct(object));
+    answer2Button.addEventListener('click', getIsCorrect);
+    answer3Button.addEventListener('click', getIsCorrect);
+    answer4Button.addEventListener('click', getIsCorrect);
 }
 
-// write questions with answers
-// make sections visible
-function question1() {
-    questionText.textContent = 'Commonly used data types do NOT include: '
-    answer1Button.textContent = 'strings';
-    answer2Button.textContent = 'booleans';
-    answer3Button.textContent = 'alerts';
-    answer4Button.textContent = 'numbers';
+function isAns1Correct(object) {
+    if (object.answer1.isCorrect) {
+        feedback.textContent = "Correct!";
+    } else {
+        feedback.textContent = "Wrong";
+    }
 }
 
-function question2() {
-    questionText.textContent = 'The condition of an if/else statement is enclosed within _____.';
-    answer1Button.textContent = 'quotes';
-    answer2Button.textContent = 'curly brackets';
-    answer3Button.textContent = 'parentheses';
-    answer4Button.textContent = 'square brackets';
-}
-
-function question3() {
-    questionText.textContent = 'Arrays in JavaScript are used to store _____.';
-    answer1Button.textContent = 'numbers and strings';
-    answer2Button.textContent = 'other arrays';
-    answer3Button.textContent = 'booleans';
-    answer4Button.textContent = 'all of the above';
-}
-
-function question4() {
-    questionText.textContent = 'String values must be enclosed within _____ when being assigned to variables.';
-    answer1Button.textContent = 'commas';
-    answer2Button.textContent = 'curly brackets';
-    answer3Button.textContent = 'quotes';
-    answer4Button.textContent = 'parentheses';
-}
-
-function question5() {
-    questionText.textContent = 'A useful tool used during development and debugging for printing content to the debugger is:'
-    answer1Button.textContent = 'JavaScript';
-    answer2Button.textContent = 'terminal/bash';
-    answer3Button.textContent = 'for loops';
-    answer4Button.textContent = 'console.log';
-}
-// write logic for scoring quiz
 
 
-// add eventlisteners
+displayWelcomePage();
 startQuizButton.addEventListener('click', timer);
+displayQuestion(question1);
